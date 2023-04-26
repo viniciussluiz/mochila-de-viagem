e2d252db997e1478fdf0c866586e75617f517a94
viniciussluiz <103579574+viniciussluiz@users.noreply.github.com>
2023-04-25 23:33:10 -0300

    Criando validações

--- js/main.js
+++ js/main.js
@@ -1,27 +1,21 @@
 const form = document.getElementById("novoItem")
+const lista = document.getElementById("lista")
 
 form.addEventListener("submit", (evento) => {
     evento.preventDefault()
 
-    criaElemento(evento.target.elements['nome'].value, evento.target['quantidade'].value)
+    criaElemento(evento.target.elements['nome'].value, evento.target.elements['quantidade'].value)
 })
+
 function criaElemento(nome, quantidade) {
-    console.log(nome)
-    console.log(quantidade)
+    const novoItem = document.createElement('li')
+    novoItem.classList.add("item")
+
+    const numeroItem = document.createElement('strong')
+    numeroItem.innerHTML = quantidade
+
+    novoItem.appendChild(numeroItem)
+    novoItem.innerHTML += nome
+
+    lista.appendChild(novoItem)
 }
-
-//<li className="item"><strong>7</strong>
-const novoItem = document.createElement('li')
-novoItem.classList.add("item")
-
-const numeroItem = document.createElement('strong')
-numeroItem.innerHTML = quantidade
-
-novoItem.appendChild(numeroItem)
-novoItem.innerHTML += nome
-
-const lista = document.getElementById("lista")
-
-lista.appendChild(novoItem)
-
-console.log(novoItem)
